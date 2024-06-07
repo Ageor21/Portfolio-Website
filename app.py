0b8ebc149f1d6e8ded6164e680a95571c5c152ea
Aaron George <ageor88@wgu.edu>
2024-06-07 02:49:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -38,6 +38,11 @@ projects = [
 @app.route("/")
 def portfolio():
     return render_template('home.html', names_projects=projects)
+
+@app.route("/button")
+def button():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
